e4a1c269a2695d97511b4db988fcb8dee4055366
Alan Malloy <alan@malloys.org>
2012-03-12 14:21:33 -0700

    checksum deps

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Define and compose schemas for describing data types, and check data against them."
   :url "https://github.com/flatland/schematic"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [useful "0.8.0-alpha1"]])
+                 [useful "0.8.0-alpha1"]]
+  :checksum-deps true)
