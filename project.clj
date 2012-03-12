7d61f7a0db45e5b8c893ea2ebf5629eb046ca305
Alan Malloy <alan@malloys.org>
2012-03-12 14:27:37 -0700

    0.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject schematic "0.0.1"
+(defproject schematic "0.0.2"
   :description "Define and compose schemas for describing data types, and check data against them."
   :url "https://github.com/flatland/schematic"
   :dependencies [[org.clojure/clojure "1.3.0"]
