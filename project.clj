b8e2dbbb76d8c90d661967d0bfb844f292a982fd
Justin Balthrop <git@justinbalthrop.com>
2012-11-16 11:20:55 -0800

    0.1.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject org.flatland/schematic "0.1.0-SNAPSHOT"
+(defproject org.flatland/schematic "0.1.0"
   :description "Define and compose schemas for describing data types, and check data against them."
   :url "https://github.com/flatland/schematic"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.flatland/useful "0.8.9-SNAPSHOT"]])
+                 [org.flatland/useful "0.9.0"]])
