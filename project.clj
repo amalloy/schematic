bffe06dd3e979bd557258ccc47df51a2e1c8acc0
Justin Balthrop <git@justinbalthrop.com>
2012-11-16 11:22:12 -0800

    add license

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject org.flatland/schematic "0.1.0"
   :description "Define and compose schemas for describing data types, and check data against them."
   :url "https://github.com/flatland/schematic"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.flatland/useful "0.9.0"]])
